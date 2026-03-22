6828d0ace028e900e15b48896c86dc8e39f93136
Kishor <kishorkhati321@gmail.com>
2026-03-22 14:25:43 +0545

    chore: remove unused Request imports in controllers

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
-use Illuminate\Http\Request;
 use App\Http\Requests\Admin\ProductStoreRequest;
 use App\Http\Requests\Admin\ProductUpdateRequest;
 use App\Traits\HandlesYouTubeUrls;
--- app/Http/Controllers/Admin/ServiceController.php
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Service;
-use Illuminate\Http\Request;
 use App\Http\Requests\Admin\ServiceStoreRequest;
 use App\Http\Requests\Admin\ServiceUpdateRequest;
 use App\Traits\HandlesYouTubeUrls;
